b8dfe4d9b11abeb6486478d6de00a6b65d300c5c
lucassilva-87 <lucas.florindo@fatec.sp.gov.br>
2025-03-06 22:41:51 -0300

    Aula de 06/03

---
+++ src/main/java/com/programacao/web/fatec/api_fatec/Controllers/ClienteController.java
@@ -0,0 +1,22 @@
+package com.programacao.web.fatec.api_fatec.Controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programacao.web.fatec.api_fatec.entities.Cliente;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteController {
+
+    //http://localhost:8080/api/cliente/criarCliente => POST
+    @PostMapping("/criarCliente")
+    public String criarCliente(@RequestBody Cliente cliente){
+
+        return "O Cliente "+cliente.getNome()+ " de idade "+cliente.getIdade()+" foi criado.";
+    }
+
+   
+}
